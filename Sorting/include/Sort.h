80e88e1432973ea39400d2eb1dd010c49c0c8938
shmile03 <denishumilin03@gmail.com>
2023-03-09 23:00:31 +0000

    added_sorting

--- Sorting/include/Sort.h
+++ Sorting/include/Sort.h
@@ -14,6 +14,8 @@ public:
 	Sort() {};
 
 	static void bubbleSort(std::vector<T>& a);
+    static void insertionSort(std::vector<T>& a);
+    static void selectionSort(std::vector<T>& a);
 };
 
 template<class T>
@@ -32,4 +34,31 @@ void Sort<T>::bubbleSort(std::vector<T>& a) {
 			}
 		}
 	}
+}
+
+template<class T>
+void Sort<T>::insertionSort(std::vector<T>& a) {
+    T tmp;
+    int j = 0;
+    for (int i = 1; i < a.size(); ++i) {
+        tmp = a[i];
+        for (j = i - 1; j >=0 && (a[j] > tmp); --j) {
+            a[j + 1] = a[j];
+        }
+        a[j + 1] = tmp;
+    }
+}
+
+template<class T>
+void Sort<T>::selectionSort(std::vector<T>& a) {
+    T tmpMin = 0;
+    for (int i = 0; i < a.size() - 1; ++i) {
+        tmpMin = i;
+        for (int j = i + 1; j < a.size(); ++j) {
+            if (a[j] < a[tmpMin]) {
+                tmpMin = j;
+            }
+        }
+        swap(&a[tmpMin], &a[i]);
+    }
 }
--- Sorting/test/test_Sort.cpp
+++ Sorting/test/test_Sort.cpp
@@ -28,3 +28,51 @@ TEST(Sort, bubble_sorting_happened) {
 
 	EXPECT_EQ(true, flag);
 }
+
+TEST(Sort, insertion_sorting_happened) {
+
+    std::vector<int> vector(LENGTH);
+
+    srand(time(NULL));
+    int tmp = 0;
+    for (int i = 0; i < LENGTH; ++i) {
+        tmp = rand() % 1000;
+        vector.push_back(tmp);
+    }
+
+    Sort<int>::insertionSort(vector);
+
+    bool flag = true;
+    for (int i = 0; i < LENGTH - 1; ++i) {
+        if (vector[i + 1] < vector[i]) {
+            flag = false;
+            break;
+        }
+    }
+
+    EXPECT_EQ(true, flag);
+}
+
+TEST(Sort, selection_sorting_happened) {
+
+    std::vector<int> vector(LENGTH);
+
+    srand(time(NULL));
+    int tmp = 0;
+    for (int i = 0; i < LENGTH; ++i) {
+        tmp = rand() % 1000;
+        vector.push_back(tmp);
+    }
+
+    Sort<int>::selectionSort(vector);
+
+    bool flag = true;
+    for (int i = 0; i < LENGTH - 1; ++i) {
+        if (vector[i + 1] < vector[i]) {
+            flag = false;
+            break;
+        }
+    }
+
+    EXPECT_EQ(true, flag);
+}
